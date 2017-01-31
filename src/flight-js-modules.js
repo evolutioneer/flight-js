5b183d8f46397bc6780c07f5e68d0f1a9b906933
Ross Smith <evolutioneer@gmail.com>
2017-01-30 22:07:44 -0500

    Delta scraper is now working for direct flights

--- src/flight-js-modules.js
+++ src/flight-js-modules.js
@@ -7,10 +7,27 @@ module.exports = {
     }
   },
   delta: {
+    // scraperjs scraper function
     scraper: function($) {
-      debug('-----------> in the scraper');
-      return $('title').text();
+      return $('#flightschedulesResults tbody.schedulesTableBody tr').map(function() {
+        var $row = $(this),
+          row = {
+            fNum: getText.call($(this), 'th form a'),
+            dPort: getText.call($(this), ['td.schedulesTableCell', 0], 'a span.schedules_airport_code'),
+            dDate: getText.call($(this), ['td.schedulesTableCell', 1], 'span'),
+            aPort: getText.call($(this), ['td.schedulesTableCell', 2], 'a span.schedules_airport_code'),
+            aDate: getText.call($(this), ['td.schedulesTableCell', 3], 'span'),
+            craft: getText.call($(this), ['td.schedulesTableCell', 4], 'span'),
+            tripTime: getText.call($(this), ['td.schedulesTableCell', 9], 'span'),
+            status: getText.call($(this), ['td.schedulesTableCell', 10])
+          };
+
+        debug(JSON.stringify(row));
+        return row;
+      }).get();
     },
+
+    // HTTP request parameters
     method: 'POST',
     url: 'http://www.delta.com/flightinfo/viewFlightStatus.action',
     headers: {
@@ -32,3 +49,34 @@ module.exports = {
   }
 };
 
+// Handles some common tasks with locating and trimming values
+function getText() {
+
+  var that = this;
+
+  for(var i = 0; i < arguments.length; i++) {
+    var a = arguments[i];
+
+    if(typeof a == 'object' && a.constructor === Array) {
+      if(a.length > 1) {
+        that = that.find(a[0]).eq(a[1]);
+      }
+
+      else {
+        a = a[0];
+      }
+    }
+
+    if(typeof a == 'string') {
+      that = that.find(a);
+    }
+  }
+
+  return that
+    .contents()
+    .filter(function() {
+      return this.nodeType === 3;
+    })
+    .text()
+    .replace(/[\t\r\n ]+/g, ' ');
+}
--- src/test.sh
+++ src/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-DEBUG=flight-js* node flight-js.js -f DTW -t SEA -c delta -d 2017-01-30
+DEBUG=flight-js* node flight-js.js -f DTW -t SEA -c delta -d 2017-01-31
 
